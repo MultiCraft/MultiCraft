2f9205c23db772489a67e8807445f3e18025bacc
MoNTE48 <MoNTE48@mail.ua>
2020-08-28 18:27:49 +0200

    License upgrade to GNU Lesser General Public License 3.0+

--- src/mapgen/mapgen_valleys.cpp
+++ src/mapgen/mapgen_valleys.cpp
@@ -11,7 +11,7 @@ Licensing changed by permission of Gael de Sailly.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation; either version 2.1 of the License, or
+the Free Software Foundation; either version 3.0 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
