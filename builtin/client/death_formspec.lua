5d1b891c6ba142892e1e1476915640fbd733236b
MoNTE48 <MoNTE48@mail.ua>
2021-09-12 16:27:25 +0200

    Align text in death formspec

--- builtin/client/death_formspec.lua
+++ builtin/client/death_formspec.lua
@@ -4,13 +4,13 @@
 core.register_on_death(function()
 	core.display_chat_message(fgettext("You died."))
 	local formspec = "size[11,5.5]bgcolor[#320000b4;true]" ..
-		"label[5,2;" .. fgettext("You died.") ..
-		"]button_exit[3.5,3;4,0.5;btn_respawn;".. fgettext("Respawn") .."]"
+		"image_button[2,2;7,0.8;blank.png;;" .. fgettext("You died.") .. ";false;false;]" ..
+		"button_exit[3.5,3;4,0.5;btn_respawn;".. fgettext("Respawn") .."]"
 	core.show_formspec("bultin:death", formspec)
 end)
 
 core.register_on_formspec_input(function(formname, fields)
-	if formname == "bultin:death" then
+	if formname == "bultin:death" and fields.btn_respawn then
 		core.send_respawn()
 	end
 end)
