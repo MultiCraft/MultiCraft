27f5248c88f5eeac86aa25d25b742601fc04dce9
Maksim <MoNTE48@mail.ua>
2022-02-18 23:42:41 +0100

    Update porting_android.cpp

--- src/porting_android.cpp
+++ src/porting_android.cpp
@@ -387,6 +387,6 @@ float getTotalSystemMemory() {
 	long pages = sysconf(_SC_PHYS_PAGES);
 	long page_size = sysconf(_SC_PAGE_SIZE);
 	int divisor = 1024 * 1024 * 1024;
-	return pages * page_size / float(divisor);
+	return pages * page_size / (float) divisor;
 }
 }
