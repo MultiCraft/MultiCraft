9acff51b737b8973883ece5a15839776a0b29421
luk3yx <luk3yx@users.noreply.github.com>
2022-06-16 20:52:41 +1200

    Use hypertext and change update info file format

--- builtin/mainmenu/dlg_version_info.lua
+++ builtin/mainmenu/dlg_version_info.lua
@@ -18,16 +18,17 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ]]
 
-local minetest_url -- Filled by HTTP callback
+local update_download_url -- Filled by HTTP callback
 
 local defaulttexturedir = core.formspec_escape(defaulttexturedir)
 
-local function version_info_formspec()
+local function version_info_formspec(data)
+	print(dump(data.changelog))
 	return ([[
 		style_type[image_button;content_offset=0]
-		image[4.9,0.3;2.5,2.5;%s]
-		image_button[1,2.5;10,0.8;%s;;%s;false;false]
-		image_button[1,3.2;10,0.8;%s;;%s;false;false]
+		image[4.9,0;2.5,2.5;%s]
+		image_button[1,2.2;10,0.8;%s;;%s;false;false]
+		hypertext[1.3,3;10,1.5;;<center>%s</center>]
 		style[version_check_remind;bgcolor=yellow]
 		button[2,4.5;4,0.8;version_check_remind;%s]
 		style[version_check_visit;bgcolor=green]
@@ -36,8 +37,7 @@ local function version_info_formspec()
 		defaulttexturedir .. "logo.png",
 		defaulttexturedir .. "blank.png",
 		fgettext("A new MultiCraft version is available!"),
-		defaulttexturedir .. "blank.png",
-		fgettext("Updating is strongly recommended."),
+		core.formspec_escape(data.changelog),
 		fgettext("Remind me later"),
 		fgettext("Update now")
 	)
@@ -56,7 +56,7 @@ local function version_info_buttonhandler(this, fields)
 	-- 	return true
 	-- end
 	if fields.version_check_visit then
-		core.open_url(minetest_url)
+		core.open_url(update_download_url)
 		-- this:delete()
 		-- return true
 	end
@@ -65,51 +65,50 @@ local function version_info_buttonhandler(this, fields)
 end
 
 
-function create_version_info_dlg(new_version)
-	assert(type(new_version) == "string")
+function create_version_info_dlg(changelog)
+	assert(type(changelog) == "string")
 
 	local retval = dialog_create("version_info",
 		version_info_formspec,
 		version_info_buttonhandler,
 		nil, true)
-	retval.data.new_version = new_version
+	retval.data.changelog = changelog
 
 	return retval
 end
 
-local function get_current_version_code()
+local function get_version_code(version_string)
 	-- Format: Major.Minor.Patch
 	-- Convert to MMMNNNPPP
-	local cur_string = core.get_version().string
-	local cur_major, cur_minor, cur_patch = cur_string:match("^(%d+).(%d+).(%d+)")
+	local ver_major, ver_minor, ver_patch = version_string:match("^(%d+).(%d+).(%d+)")
 
-	if not cur_patch then
+	if not ver_patch then
 		core.log("error", "Failed to read version numbers (invalid tag format?)")
 		return
 	end
 
-	return (cur_major * 1000 + cur_minor) * 1000 + cur_patch
+	return (ver_major * 1000 + ver_minor) * 1000 + ver_patch
 end
 
-local function on_version_info_received(json)
-	minetest_url = json.latest["url_" .. PLATFORM:lower()] or json.latest.url
-
+local function on_version_info_received(update_info)
 	local known_update = tonumber(core.settings:get("update_last_known")) or 0
 
-	-- Format: MMNNPPP (Major, Minor, Patch)
-	local new_number = json.latest.version_code
+	-- Parse the latest version number
+	local new_number = get_version_code(update_info.latest_version or "")
 	if not new_number then
-		core.log("error", "Failed to read version numbers (invalid tag format?)")
+		core.log("error", "dlg_version_info.lua: Failed to read version number")
 		return
 	end
 
-	local cur_number = get_current_version_code()
-	if new_number <= known_update or new_number < cur_number then
+	-- Compare with the current number
+	local client_version = core.get_version()
+	local cur_number = get_version_code(client_version.string)
+	if not cur_number or new_number <= known_update or new_number < cur_number then
 		return
 	end
 
 	-- Also consider updating from 1.2.3-dev to 1.2.3
-	if new_number == cur_number and not core.get_version().is_dev then
+	if new_number == cur_number and not client_version.is_dev then
 		return
 	end
 
@@ -118,7 +117,8 @@ local function on_version_info_received(json)
 	local tabs = ui.find_by_name("maintab")
 	tabs:hide()
 
-	local version_info_dlg = create_version_info_dlg(json.latest.version)
+	update_download_url = update_info.url or "https://multicraft.world/downloads"
+	local version_info_dlg = create_version_info_dlg(update_info.changelog or "")
 	version_info_dlg:set_parent(tabs)
 	version_info_dlg:show()
 
@@ -147,12 +147,15 @@ function check_new_version()
 		return http.fetch_sync(params)
 	end, { url = url }, function(result)
 		local json = result.succeeded and core.parse_json(result.data)
-		if type(json) ~= "table" or not json.latest then
+		if type(json) ~= "table" then
 			core.log("error", "Failed to read JSON output from " .. url ..
 					", status code = " .. result.code)
 			return
+		elseif not json[PLATFORM] or not json[PLATFORM].latest_version then
+			-- No platform-specific update information, don't display anything
+			return
 		end
 
-		on_version_info_received(json)
+		on_version_info_received(json[PLATFORM])
 	end)
 end
