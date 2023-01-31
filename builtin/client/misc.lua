ef6a478ce2272a6be5f0a2fba3daebe60569d6b4
luk3yx <luk3yx@users.noreply.github.com>
2023-02-01 10:22:31 +1300

    Add .fly and .pitch

--- builtin/client/misc.lua
+++ builtin/client/misc.lua
@@ -6,21 +6,23 @@ function core.setting_get_pos(name)
 	return core.string_to_pos(value)
 end
 
-local function register_toggle_cmd(name, priv, setting)
-	core.register_chatcommand(priv, {
+local function register_toggle_cmd(name, cmd, setting, no_priv)
+	core.register_chatcommand(cmd, {
 		description = "Toggles " .. name,
 		func = function()
 			if core.settings:get_bool(setting) then
 				core.settings:set_bool(setting, false)
 				return true, "Disabled " .. name
-			elseif core.get_privilege_list()[priv] then
+			elseif no_priv or core.get_privilege_list()[cmd] then
 				core.settings:set_bool(setting, true)
 				return true, "Enabled " .. name
 			end
-			return false, "You do not have the " .. priv .. " privilege."
+			return false, "You do not have the " .. cmd .. " privilege."
 		end,
 	})
 end
 
 register_toggle_cmd("fast mode", "fast", "fast_move")
 register_toggle_cmd("noclip mode", "noclip", "noclip")
+register_toggle_cmd("fly mode", "fly", "free_move")
+register_toggle_cmd("pitch move mode", "pitch", "pitch_move", true)
