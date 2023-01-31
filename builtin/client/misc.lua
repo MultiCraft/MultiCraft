61285a496240541f17a342c46dca13d72e3f019d
luk3yx <luk3yx@users.noreply.github.com>
2023-01-31 20:37:49 +1300

    Add .fast and .noclip chatcommands

--- builtin/client/misc.lua
+++ builtin/client/misc.lua
@@ -5,3 +5,22 @@ function core.setting_get_pos(name)
 	end
 	return core.string_to_pos(value)
 end
+
+local function register_toggle_cmd(name, priv, setting)
+	core.register_chatcommand(priv, {
+		description = "Toggles " .. name,
+		func = function()
+			if core.settings:get_bool(setting) then
+				core.settings:set_bool(setting, false)
+				return true, "Disabled " .. name
+			elseif core.get_privilege_list()[priv] then
+				core.settings:set_bool(setting, true)
+				return true, "Enabled " .. name
+			end
+			return false, "You do not have the " .. priv .. " privilege."
+		end,
+	})
+end
+
+register_toggle_cmd("fast mode", "fast", "fast_move")
+register_toggle_cmd("noclip mode", "noclip", "noclip")
