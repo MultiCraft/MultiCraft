3a1ae116e9646c34685e34fa0aca95927dd8c9fa
Deve <deveee@gmail.com>
2023-05-16 23:44:02 +0200

    Move update hovered element to separate pull request

--- src/gui/modalMenu.cpp
+++ src/gui/modalMenu.cpp
@@ -159,17 +159,7 @@ void GUIModalMenu::quitMenu()
 	// This removes Environment's grab on us
 	Environment->removeFocus(this);
 	m_menumgr->deletingMenu(this);
-	this->grab();
 	this->remove();
-
-#if IRRLICHT_VERSION_MAJOR == 1 && IRRLICHT_VERSION_MINOR >= 9
-	// Force update hovered elements, so that GUI Environment drops previously
-	// grabbed elements that are not hovered anymore
-	Environment->forceUpdateHoveredElement();
-#endif
-
-	this->drop();
-
 #ifdef HAVE_TOUCHSCREENGUI
 	if (g_touchscreengui && g_touchscreengui->isActive() && m_touchscreen_visible)
 		g_touchscreengui->show();
