97526456db067d9088e8caebfe3179e0aff5deaa
Maksym H <Maksym48@pm.me>
2023-07-31 22:08:50 +0300

    Update build.yml

--- Windows/deps/sdk.sh
+++ Windows/deps/sdk.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 
+export CC=clang
+export CXX=clang++
+
 export CFLAGS="-fvisibility=hidden -fexceptions -O3"
 export CXXFLAGS="$CFLAGS -frtti -O3"
 
-export NPROC=`(nproc) + 1`
+export NPROC=$(( $(nproc) + 1 ))
