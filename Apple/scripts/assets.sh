a269f7907ee987ad26dcdcccd1077d47bc1899a9
Maksym H <Maksym48@pm.me>
2024-02-02 19:54:17 +0100

    Update font

--- Apple/scripts/assets.sh
+++ Apple/scripts/assets.sh
@@ -16,5 +16,5 @@ then
 	cp ../fonts/*.ttf $DEST/fonts/
 else
 	cp ../fonts/MultiCraftFont.ttf $DEST/fonts/
-	cp ../fonts/NotoEmoji-Regular.ttf $DEST/fonts/
+	cp ../fonts/NotoEmoji.ttf $DEST/fonts/
 fi
