96533259ea1acb7a4516d7e47faac534f1869ff2
Deve <deveee@gmail.com>
2025-02-24 22:58:37 +0100

    Use apple library to generate sha1 digest

--- src/util/hashing.cpp
+++ src/util/hashing.cpp
@@ -9,6 +9,8 @@
 #if USE_OPENSSL
 #include <openssl/sha.h>
 #include <openssl/evp.h>
+#elif defined(__APPLE__)
+#include <CommonCrypto/CommonDigest.h>
 #else
 #include "util/sha1.h"
 #endif
@@ -23,6 +25,13 @@ std::string sha1(const std::string &data)
 	auto src = reinterpret_cast<const uint8_t *>(data.data());
 	auto dst = reinterpret_cast<uint8_t *>(&digest[0]);
 	SHA1(src, data.size(), dst);
+	return digest;
+#elif defined(__APPLE__)
+	std::string digest(CC_SHA1_DIGEST_LENGTH, '\0');
+	CC_SHA1_CTX context;
+	CC_SHA1_Init(&context);
+	CC_SHA1_Update(&context, data.data(), data.size());
+	CC_SHA1_Final(reinterpret_cast<unsigned char *>(&digest[0]), &context);
 	return digest;
 #else
 	SHA1 sha1;
