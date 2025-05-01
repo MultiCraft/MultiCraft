934499bdfbf1e715f058518cb8c79ea883728a31
Maksym H. <Maksym48@pm.me>
2025-05-01 17:24:28 +0200

    Tune LevelDB default options for server

--- src/database/database-leveldb.cpp
+++ src/database/database-leveldb.cpp
@@ -45,6 +45,10 @@ Database_LevelDB::Database_LevelDB(const std::string &savedir)
 {
 	leveldb::Options options;
 	options.create_if_missing = true;
+#ifdef SERVER
+	options.write_buffer_size = 16 * 1024 * 1024; // x4 from default
+	options.max_file_size = 8 * 1024 * 1024; // x4 from default
+#endif
 	leveldb::Status status = leveldb::DB::Open(options,
 		savedir + DIR_DELIM + "map.db", &m_database);
 	ENSURE_STATUS_OK(status);
