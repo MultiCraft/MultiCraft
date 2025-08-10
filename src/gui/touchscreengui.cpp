a8528eeede2db0a9104ae7d5f15f19d6c52fc439
Maksym H. <Maksym48@pm.me>
2025-08-10 12:00:39 +0200

    Move close to unused

--- src/gui/touchscreengui.cpp
+++ src/gui/touchscreengui.cpp
@@ -57,7 +57,7 @@ const button_data buttons_data[] = {
 	{ "joystick_center.png", "", "", "", "joystick_center", false, -1 },
 	{ "", "", "", N_("Open editor"), "editor_open", true, -1 },
 	{ "edit_ui_save.png", "edit_ui_save_pressed.png", "", N_("Save"), "editor_save", true, -1 },
-	{ "edit_ui_close.png", "edit_ui_close_pressed.png", "", N_("Close"), "editor_close", true, -1 },
+//	{ "edit_ui_close.png", "edit_ui_close_pressed.png", "", N_("Close"), "editor_close", true, -1 },
 	{ "edit_ui_restore.png", "edit_ui_restore_pressed.png", "", N_("Restore"), "editor_default", true, -1 },
 	{ "edit_ui_move.png", "edit_ui_move_pressed.png", "", N_("Move"), "editor_move", true, -1 },
 	{ "edit_ui_scale.png", "edit_ui_scale_pressed.png", "", N_("Scale"), "editor_scale", true, -1 },
@@ -789,10 +789,10 @@ bool TouchScreenGUI::preprocessEvent(const SEvent &event)
 						play_sound = true;
 						m_settings->updateConfigFile(m_settings_path.c_str());
 						new_state = STATE_DEFAULT;
-					} else if (button->id == editor_close_id) {
-						play_sound = true;
-						restore_all_values = true;
-						new_state = STATE_DEFAULT;
+				//	} else if (button->id == editor_close_id) {
+				//		play_sound = true;
+				//		restore_all_values = true;
+				//		new_state = STATE_DEFAULT;
 					} else if (button->id == editor_default_id) {
 						play_sound = true;
 						reset_all_values = true;
