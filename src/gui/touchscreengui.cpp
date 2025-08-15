55fea47722526b68e5ee1bf0e820ba06a6e3651e
Maksym H. <Maksym48@pm.me>
2025-08-15 13:51:32 +0200

    .

--- src/gui/touchscreengui.cpp
+++ src/gui/touchscreengui.cpp
@@ -5,7 +5,7 @@ Copyright (C) 2018 srifqi, Muhammad Rifqi Priyo Susanto
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation; either version 2.1 of the License, or
+the Free Software Foundation; either version 3.0 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
