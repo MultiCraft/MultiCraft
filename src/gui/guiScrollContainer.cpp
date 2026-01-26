a5094ba7cd1971d4a31e5f7faddf7d196ca6004e
Deve <deveee@gmail.com>
2026-01-26 13:04:19 +0100

    Make scroll coasting working with gui table and hypertext (#296)

--- src/gui/guiScrollContainer.cpp
+++ src/gui/guiScrollContainer.cpp
@@ -18,7 +18,6 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 */
 
 #include "guiScrollContainer.h"
-#include "porting.h"
 
 #ifdef _IRR_COMPILE_WITH_SDL_DEVICE_
 GUIScrollContainer::GUIScrollContainer(gui::IGUIEnvironment *env,
@@ -26,13 +25,15 @@ GUIScrollContainer::GUIScrollContainer(gui::IGUIEnvironment *env,
 		const std::string &orientation, f32 scrollfactor) :
 		gui::IGUIElement(gui::EGUIET_CUSTOM_SCROLLCONTAINER, env, parent, id,
 				rectangle),
-		m_scrollbar(nullptr), m_scrollfactor(scrollfactor)
+		m_scrollbar(nullptr), m_scroll_swipe(nullptr),
+		m_scrollfactor(scrollfactor)
 #else
 GUIScrollContainer::GUIScrollContainer(gui::IGUIEnvironment *env,
 		gui::IGUIElement *parent, s32 id, const core::rect<s32> &rectangle,
 		const std::string &orientation, f32 scrollfactor) :
 		gui::IGUIElement(gui::EGUIET_ELEMENT, env, parent, id, rectangle),
-		m_scrollbar(nullptr), m_scrollfactor(scrollfactor)
+		m_scrollbar(nullptr), m_scroll_swipe(nullptr),
+		m_scrollfactor(scrollfactor)
 #endif
 {
 	if (orientation == "vertical")
@@ -41,6 +42,14 @@ GUIScrollContainer::GUIScrollContainer(gui::IGUIEnvironment *env,
 		m_orientation = HORIZONTAL;
 	else
 		m_orientation = UNDEFINED;
+
+#ifdef _IRR_COMPILE_WITH_SDL_DEVICE_
+#ifdef HAVE_TOUCHSCREENGUI
+	m_scroll_swipe = new ScrollSwipe(
+			env, this, (ScrollSwipe::OrientationEnum)m_orientation);
+	m_scroll_swipe->setScrollFactor(m_scrollfactor);
+#endif
+#endif
 }
 
 bool GUIScrollContainer::OnEvent(const SEvent &event)
@@ -63,162 +72,19 @@ bool GUIScrollContainer::OnEvent(const SEvent &event)
 		return retval;
 	}
 
-#ifdef _IRR_COMPILE_WITH_SDL_DEVICE_
-#ifdef HAVE_TOUCHSCREENGUI
-	if (event.EventType == EET_MOUSE_INPUT_EVENT && m_scrollbar) {
-		const int mouse_pos = m_orientation == HORIZONTAL ? event.MouseInput.X
-								  : event.MouseInput.Y;
-		if (event.MouseInput.Event == EMIE_LMOUSE_PRESSED_DOWN) {
-			if (isPointInside(core::position2d<s32>(
-					    event.MouseInput.X, event.MouseInput.Y))) {
-				m_swipe_start_px = mouse_pos -
-						   m_scrollbar->getPos() * m_scrollfactor;
-				m_velocity = 0;
-				m_is_coasting = false;
-				m_last_pos = m_scrollbar->getPos();
-			}
-		} else if (event.MouseInput.Event == EMIE_LMOUSE_LEFT_UP) {
-			m_swipe_start_px = -1;
-			if (m_swipe_started) {
-				m_swipe_started = false;
-				calculateCoastingVelocity();
-				return true;
-			}
-		} else if (event.MouseInput.Event == EMIE_MOUSE_MOVED) {
-			double screen_dpi = RenderingEngine::getDisplayDensity() * 96;
-
-			if (!m_swipe_started && m_orientation != UNDEFINED &&
-					m_swipe_start_px != -1 &&
-					std::abs(m_swipe_start_px - mouse_pos +
-							m_scrollbar->getPos() *
-									m_scrollfactor) >
-							0.1 * screen_dpi) {
-				m_swipe_started = true;
-				Environment->setFocus(this);
-				m_sample_count = 0;
-				m_sample_index = 0;
-			}
-
-			if (m_swipe_started) {
-				m_swipe_pos = (float)(mouse_pos - m_swipe_start_px) /
-					      m_scrollfactor;
-
-				u64 current_time = porting::getTimeMs();
-
-				m_velocity_samples[m_sample_index].position = m_swipe_pos;
-				m_velocity_samples[m_sample_index].timestamp =
-						current_time;
-				m_sample_index = (m_sample_index + 1) %
-						 MAX_VELOCITY_SAMPLES;
-				if (m_sample_count < MAX_VELOCITY_SAMPLES) {
-					m_sample_count++;
-				}
-
-				m_last_time = current_time;
-				m_scrollbar->setPos((int)m_swipe_pos);
-				SEvent e;
-				e.EventType = EET_GUI_EVENT;
-				e.GUIEvent.Caller = m_scrollbar;
-				e.GUIEvent.Element = nullptr;
-				e.GUIEvent.EventType = EGET_SCROLL_BAR_CHANGED;
-				OnEvent(e);
-
-				return true;
-			}
-		}
+	if (m_scroll_swipe) {
+		bool retval = m_scroll_swipe->onEvent(event);
+		if (retval)
+			return true;
 	}
-#endif
-#endif
 
 	return IGUIElement::OnEvent(event);
 }
 
-void GUIScrollContainer::calculateCoastingVelocity()
-{
-	if (m_sample_count < 1)
-		return;
-
-	u32 last_sample_index = (m_sample_index - 1 + MAX_VELOCITY_SAMPLES) %
-				MAX_VELOCITY_SAMPLES;
-	VelocitySample last_sample = m_velocity_samples[last_sample_index];
-
-	VelocitySample first_sample = last_sample;
-	for (u32 i = 0; i < m_sample_count; i++) {
-		VelocitySample &sample = m_velocity_samples[i];
-
-		if (m_last_time - sample.timestamp > 100)
-			continue;
-
-		if (sample.timestamp < first_sample.timestamp)
-			first_sample = sample;
-	}
-
-	u64 dt = last_sample.timestamp - first_sample.timestamp;
-	if (dt > 0) {
-		double screen_dpi = RenderingEngine::getDisplayDensity() * 96;
-		float distance = (last_sample.position - first_sample.position) *
-				 m_scrollfactor;
-		float distance_in = distance * 1000 / screen_dpi;
-
-		m_velocity = distance_in / dt;
-		m_velocity = std::max(std::min(m_velocity, 40.0f), -40.0f);
-		m_is_coasting = true;
-	}
-}
-
-void GUIScrollContainer::updateScrollCoasting()
-{
-#ifdef _IRR_COMPILE_WITH_SDL_DEVICE_
-#ifdef HAVE_TOUCHSCREENGUI
-	if (!m_is_coasting || m_velocity == 0)
-		return;
-
-	u64 current_time = porting::getTimeMs();
-	u64 dt = current_time - m_last_time;
-	m_last_time = current_time;
-
-	if (dt == 0)
-		return;
-
-	const float SLOWING_FACTOR = 0.95f;
-	const float VELOCITY_FACTOR = 1.0f;
-
-	// We want SLOWING_FACTOR to be added every frame assuming 60 FPS
-	m_velocity *= std::pow(SLOWING_FACTOR, dt / (1000.0f / 60.0f));
-	if (std::abs(m_velocity) < 0.5f) {
-		m_is_coasting = false;
-		return;
-	}
-
-	double screen_dpi = RenderingEngine::getDisplayDensity() * 96;
-	float pixel_velocity = m_velocity * screen_dpi / m_scrollfactor / 1000;
-
-	m_swipe_pos += pixel_velocity * VELOCITY_FACTOR * dt;
-
-	if (m_swipe_pos <= m_scrollbar->getMin()) {
-		m_swipe_pos = m_scrollbar->getMin();
-		m_velocity = 0;
-		m_is_coasting = false;
-	} else if (m_swipe_pos >= m_scrollbar->getMax()) {
-		m_swipe_pos = m_scrollbar->getMax();
-		m_velocity = 0;
-		m_is_coasting = false;
-	}
-
-	m_scrollbar->setPos((int)m_swipe_pos);
-	SEvent e;
-	e.EventType = EET_GUI_EVENT;
-	e.GUIEvent.Caller = m_scrollbar;
-	e.GUIEvent.Element = nullptr;
-	e.GUIEvent.EventType = EGET_SCROLL_BAR_CHANGED;
-	OnEvent(e);
-#endif
-#endif
-}
-
 void GUIScrollContainer::draw()
 {
-	updateScrollCoasting();
+	if (m_scroll_swipe)
+		m_scroll_swipe->updateScrollCoasting();
 
 	if (isVisible()) {
 		core::list<IGUIElement *>::Iterator it = Children.begin();
