830f82909d1cf7e29098b6eeda3c0925dfcbf4ea
Deve <deveee@gmail.com>
2026-01-29 23:16:24 +0100

    Build harfbuzz for apple

---
+++ Apple/scripts/harfbuzz.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+
+HARFBUZZ_VERSION=12.3.2
+
+. scripts/sdk.sh
+mkdir -p deps; cd deps
+
+if [ ! -d harfbuzz-src ]; then
+	git clone -b $HARFBUZZ_VERSION --depth 1 https://github.com/harfbuzz/harfbuzz.git harfbuzz-src
+	mkdir harfbuzz-src/build
+fi
+
+rm -rf harfbuzz
+cd harfbuzz-src/build
+
+cmake .. \
+	-DBUILD_SHARED_LIBS=FALSE \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_C_FLAGS_RELEASE="$CFLAGS" \
+	-DFREETYPE_LIBRARY="../../freetype/lib/$TARGET_ABI/libfreetype.a ../../libpng/lib/$TARGET_ABI/libpng.a" \
+	-DFREETYPE_INCLUDE_DIRS="../../freetype/include" \
+	-DHB_HAVE_GLIB=OFF \
+	-DHB_HAVE_GOBJECT=OFF \
+	-DHB_HAVE_ICU=OFF \
+	-DHB_HAVE_FREETYPE=ON \
+	-DHB_BUILD_SUBSET=OFF \
+	-DCMAKE_C_FLAGS_RELEASE="$OSX_FLAGS $OSX_ARCH" \
+	-DCMAKE_OSX_ARCHITECTURES=$OSX_ARCHITECTURES
+
+cmake --build . -j
+
+mkdir -p ../../harfbuzz/include/harfbuzz
+cp -v libharfbuzz.a ../../harfbuzz
+cp -v ../src/*.h ../../harfbuzz/include/harfbuzz
+
+echo "Freetype build successful"
