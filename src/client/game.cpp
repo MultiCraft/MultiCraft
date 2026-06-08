d235a470dd454cc5bb01a7bb111c840091b045fb
luk3yx <luk3yx@users.noreply.github.com>
2026-06-08 20:06:29 +1200

    Fix pause animations with pause_game[]
    This is a partial backport of https://github.com/luanti-org/luanti/commit/0251b01da66a67ed84f8d27c33aa35f34aaca237
    since that happens to do what I want

--- src/client/game.cpp
+++ src/client/game.cpp
@@ -744,7 +744,7 @@ protected:
 	void updateCameraDirection(CameraOrientation *cam, float dtime);
 	void updateCameraOrientation(CameraOrientation *cam, float dtime);
 	void updatePlayerControl(const CameraOrientation &cam);
-	void step(f32 *dtime);
+	void step(f32 dtime);
 	void processClientEvents(CameraOrientation *cam);
 	void updateCamera(u32 busy_time, f32 dtime);
 	void updateSound(f32 dtime);
@@ -930,6 +930,10 @@ private:
 	bool m_camera_offset_changed = false;
 
 	bool m_does_lost_focus_pause_game = false;
+
+	// if true, (almost) the whole game is paused
+	// this happens in pause menu in singleplayer
+	bool m_is_paused = false;
 
 	bool m_enable_vibrations = false;
 
@@ -1187,7 +1191,22 @@ void Game::run()
 		cam_view.camera_pitch += (cam_view_target.camera_pitch -
 				cam_view.camera_pitch) * m_cache_cam_smoothing;
 		updatePlayerControl(cam_view);
-		step(&dtime);
+
+		{
+			bool was_paused = m_is_paused;
+			m_is_paused = simple_singleplayer_mode && g_menumgr.pausesGame();
+			if (m_is_paused)
+				dtime = 0.0f;
+
+			if (!was_paused && m_is_paused)
+				pauseAnimation();
+			else if (was_paused && !m_is_paused)
+				resumeAnimation();
+		}
+
+		if (!m_is_paused)
+			step(dtime);
+
 		processClientEvents(&cam_view_target);
 		updateDebugState();
 		updateCamera(draw_times.busy_time, dtime);
@@ -2720,22 +2739,12 @@ void Game::updatePlayerControl(const CameraOrientation &cam)
 }
 
 
-inline void Game::step(f32 *dtime)
+inline void Game::step(f32 dtime)
 {
-	bool can_be_and_is_paused =
-			(simple_singleplayer_mode && g_menumgr.pausesGame());
+	if (server)
+		server->step(dtime);
 
-	if (can_be_and_is_paused) { // This is for a singleplayer server
-		*dtime = 0;             // No time passes
-	} else {
-		if (simple_singleplayer_mode && !paused_animated_nodes.empty())
-			resumeAnimation();
-
-		if (server)
-			server->step(*dtime);
-
-		client->step(*dtime);
-	}
+	client->step(dtime);
 }
 
 static void pauseNodeAnimation(PausedNodesList &paused, scene::ISceneNode *node) {
@@ -4707,9 +4716,6 @@ void Game::showPauseMenu()
 		GUIFormSpecMenu::create(formspec, client, &input->joystick,
 				fs_src, txt_dst, client->getFormspecPrepend(), sound);
 	}
-
-	if (simple_singleplayer_mode)
-		pauseAnimation();
 }
 
 void Game::showChangePasswordDialog(std::string old_pw, std::string new_pw,
