23bc75c8c1c1ae5e2048b5c4c2d0321a3957fb12
BlackImpostor <106878493+SkyBuilder1717@users.noreply.github.com>
2026-06-23 12:39:16 +0300

    Fix gerBrightness

--- src/client/tile.cpp
+++ src/client/tile.cpp
@@ -2142,7 +2142,7 @@ static void apply_hue_saturation(video::IImage *dst, v2u32 dst_pos, v2u32 size,
 		for (u32 x = dst_pos.X; x < dst_pos.X + size.X; x++) {
 
 			if (colorize) {
-				f32 lum = dst->getPixel(x, y).getBrightness() / 255.0f;
+				f32 lum = dst->getPixel(x, y).getLightness() / 255.0f;
 
 				if (norm_l < 0) {
 					lum *= norm_l + 1.0f;
