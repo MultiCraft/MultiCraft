0dee7f2b2a817bf97c3e57df9d3f7dbc9ac05309
Maksym H. <Maksym48@pm.me>
2026-06-27 00:49:09 +0200

    Switch to `porting::secure_rand_fill_buf`

--- src/util/encryption.cpp
+++ src/util/encryption.cpp
@@ -23,37 +23,14 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <random>
 
-#if USE_OPENSSL
-#include <openssl/rand.h>
-#elif defined(__APPLE__)
-#include <CommonCrypto/CommonRandom.h>
-#endif
-
-#if defined(__ANDROID__) || defined(__APPLE__)
-#include <porting.h>
-#endif
+#include "porting.h"
 
 uint8_t Encryption::key[32] = {};
 
 void Encryption::generateSalt(unsigned char *salt, unsigned int size)
 {
-#if USE_OPENSSL
-	if (RAND_bytes(salt, (int)size) == 1)
-		return;
-		// Fall back to std::random_device if libcrypto's RNG is unavailable
-#elif defined(__APPLE__)
-	if (CCRandomGenerateBytes(salt, size) == kCCSuccess)
-		return;
-		// Fall back to std::random_device if CommonCrypto's RNG is unavailable
-#endif
-
-	std::random_device rd;
-
-	for (unsigned int i = 0; i < size; i++) {
-		salt[i] = static_cast<unsigned char>(rd());
-	}
+	porting::secure_rand_fill_buf(salt, size);
 }
 
 void Encryption::hmacInit(SHA256_CTX *ctx, const uint8_t *key)
