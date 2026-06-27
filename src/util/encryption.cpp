98e38ddf3b5a125aae3ac2939e3c75ca2a89023c
Maksym H. <Maksym48@pm.me>
2026-06-27 10:25:23 +0200

    Update encryption.cpp

--- src/util/encryption.cpp
+++ src/util/encryption.cpp
@@ -17,14 +17,13 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+#include "porting.h"
 #include "encryption.h"
 #include "ecrypt-sync.h"
 
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-
-#include "porting.h"
 
 uint8_t Encryption::key[32] = {};
 
